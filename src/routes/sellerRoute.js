16c3239f7acf14264c579c352cef9f049c2b7b77
elitedigitals <eimiuhiehijie@gmail.com>
2025-09-18 20:13:37 +0100

    aa

--- src/routes/sellerRoute.js
+++ src/routes/sellerRoute.js
@@ -11,21 +11,21 @@ const router = express.Router();
 router.post("/register", sellerSignup);
 
 
-router.get("/dashboard", protect, authorize("seller"), (req, res) => {
+router.get("/dashboard", protect, authorize("Seller"), (req, res) => {
   res.status(200).json({ message: "Welcome to the seller dashboard!" });
 });
 
 //get seller profile
-router.get("/profile", protect, authorize("seller"), getSellerProfile);
+router.get("/profile", protect, authorize("Seller"), getSellerProfile);
 
 //update seller profile
 router.put("/update-profile", protect, authorize("seller"), updateSellerProfile);
 
 
 //delete seller account
-router.delete("/delete-account", protect, authorize("seller"), deleteSellerProfile);
+router.delete("/delete-account", protect, authorize("Seller"), deleteSellerProfile);
 
 
 //add sellers bank details
-router.post("/add-bank-details", protect, authorize("seller"), saveBankDetails);
+router.post("/add-bank-details", protect, authorize("Seller"), saveBankDetails);
 export default router;
